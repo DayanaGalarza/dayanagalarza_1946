ce8f994562dd103293d8385fa24e48d202d1223c
Dayana Galarza <daygal08@gmail.com>
2025-02-16 17:15:05 -0500

    TallerString

--- STRING/Tallerentregable.cpp
+++ STRING/Tallerentregable.cpp
@@ -3,24 +3,42 @@
 using namespace std;
 
 void reemplazarPalabra(string &cadena, const string &buscar, const string &reemplazar) {
-    size_t pos = cadena.find(buscar);
-    if (pos != string::npos) {
-        cadena.replace(pos, buscar.length(), reemplazar);
-    } else {
-        cout << "No se encontró la palabra o frase a reemplazar." << endl;
+    size_t pos = 0;
+    size_t len = cadena.length();
+    
+    while (pos < len) {
+
+        pos = cadena.find(buscar, pos);
+        if (pos == string::npos) break;
+        
+        bool esInicio = (pos == 0 || !isalpha(cadena[pos-1]));
+        bool esFinal = (pos + buscar.length() == len || !isalpha(cadena[pos + buscar.length()]));
+        
+        if (esInicio && esFinal) {
+            cadena.replace(pos, buscar.length(), reemplazar);
+            pos += reemplazar.length();
+            len = cadena.length(); 
+        } else {
+            // Si no es palabra completa, seguir buscando
+            pos += 1;
+        }
     }
 }
 
-int main() 
-{
+int main() {
     string cadena, buscar, reemplazar;
+    
     cout << "Ingrese la cadena principal: ";
     getline(cin, cadena);
+    
     cout << "Ingrese la palabra que desea reemplazar: ";
     getline(cin, buscar);
+    
     cout << "Ingrese la nueva palabra o frase: ";
     getline(cin, reemplazar);
+    
     reemplazarPalabra(cadena, buscar, reemplazar);
     cout << "Cadena modificada: " << cadena << endl;
-
+    
+    return 0;
 }
